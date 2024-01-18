10405da3394121b8bd414745311d26507559a8a4
Your Name <you@example.com>
2024-01-18 17:35:33 +0000

    copied

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof\(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof\(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)si\zeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned lo\ng)sizeof(lli));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeo\f(f));
-
-	return (0);
+        int i;
+        long int li;
+        long long int lli;
+        float f;
+        char c;
+        printf("Size of a char: %lu byte\(s\)\n", (unsigned long)size\of(c))
+;
+        printf("Size of an int: %lu byte\(s\)\n", (unsigned long)size\of(i))
+;
+        printf("Size of a long int: %lu byte\(s\)\n", (unsigned long)\sizeof(li));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned lo\ng)sizeof(lli));
+        printf("Size of a float: %lu byte\(s\)\n", (unsigned long)siz\eof(f)
+);
+        return (0);
 }
