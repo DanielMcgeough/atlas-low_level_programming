c4ebd0478885f184c74daf39cd4321166581156e
Your Name <you@example.com>
2024-01-18 19:47:03 +0000

    added comment

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/** more headers goes there
+* main - we are using if statements to print some stuff.
+* betty style doc for function main goes there 
+*/
 int main(void)
 {
 	int n;
