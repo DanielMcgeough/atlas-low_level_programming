970223bbeddd19c90b7ca37b3c384a24092e066e
Your Name <you@example.com>
2024-01-18 17:22:08 +0000

    added and activated 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - use sizeof command, betty, you suck.
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("Size of a char: %lu c byte(s)\n", (unsigned long)size\of(c));
+	printf("Size of an int: %lu i byte(s)\n", (unsigned long)size\of(i));
+	printf("Size of a long int: %lu li byte(s)\n", (unsigned long\)sizeof(li));
+	printf("Size of a long long int: %lu lli byte(s)\n", (unsigne\d long)sizeof(lli));
+	printf("Size of a float: %lu f byte(s)\n", (unsigned long)siz\eof(float));
+	return (0);
+}
