cb7b7dcee9e1b5b9423de52da5d677abe53f3b5c
Your Name <you@example.com>
2024-01-18 17:24:33 +0000

    added spaces

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,12 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %lu c byte(s)\n", (unsigned long)size\of(c));
 	printf("Size of an int: %lu i byte(s)\n", (unsigned long)size\of(i));
 	printf("Size of a long int: %lu li byte(s)\n", (unsigned long\)sizeof(li));
 	printf("Size of a long long int: %lu lli byte(s)\n", (unsigne\d long)sizeof(lli));
 	printf("Size of a float: %lu f byte(s)\n", (unsigned long)siz\eof(float));
+
 	return (0);
 }
