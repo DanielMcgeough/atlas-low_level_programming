6712cdcb75f163c99d602949a7779ab0c6de5516
Your Name <you@example.com>
2024-01-18 19:08:19 +0000

    added some brackets

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0);
+	{
 		printf(" is positive");
+	}
 	if else (n = 0);
+	{
 		printf(" is zero");
+	}
 	else 
+	{
 		printf(" is negative");
+	}
 	return (0);
 }
