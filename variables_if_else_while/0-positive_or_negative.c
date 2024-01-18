1321c1c90ce1a66723ab4e60f2edf429c92149b2
Your Name <you@example.com>
2024-01-18 19:10:46 +0000

    removed some stuff

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0);
+	if (n > 0)
 	{
 		printf(" is positive");
 	}
-	if else (n = 0);
+	if else (n = 0)
 	{
 		printf(" is zero");
 	}
