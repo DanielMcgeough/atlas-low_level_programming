3af28d2d93b3208334f65ac122700dac0fc43683
Your Name <you@example.com>
2024-01-19 17:56:37 +0000

    added and activated

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	char a = '0'
+
+	while ( a<= '10')
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+
+	return (0);
+}
