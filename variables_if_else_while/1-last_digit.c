f283b43f8add3e7ed632ae162ef3c6b6114d1267
Your Name <you@example.com>
2024-01-19 16:37:42 +0000

    revert

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@ int main(void)
         int n;
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-	echo("Last digit of %d is %d", n, n % 10);
+	printf("Last digit of %d is %d", n, n % 10);
         if (n % 10 > 5)
         {
-            	echo(" and is greater than 5");
+            	printf(" and is greater than 5");
         }
         else if (n % 10 == 0)
         {
-            	echo(" and is 0");
+            	printf(" and is 0");
         }
         else
         {
-        	echo(" and is less than 6 and not 0");
+        	printf(" and is less than 6 and not 0");
         }
 
         return (0);
