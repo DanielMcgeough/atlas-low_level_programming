ad452e7db7790341f0674e78a7be3d2ef820001b
Your Name <you@example.com>
2024-01-19 15:16:14 +0000

    fix formatting errors

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,17 +8,17 @@ int main(void)
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         printf("Last digit of %d is %d/10",(n),(n));
-        if ("%d" % 10 > 5);
-          {
-            printf("and is greater than 5");
-              }
-        else if ("%d" % 10 == 0,(n));
-          {
-            printf("and is 0");
-              }
+        if ("%d" % 10 > 5)
+        {
+            	printf("and is greater than 5");
+        }
+        else if ("%d" % 10 == 0,(n))
+        {
+            	printf("and is 0");
+        }
         else
-          {
-            printf("and is less than 6 and not 0");
-              }
+        {
+        	printf("and is less than 6 and not 0");
+        }
         return (0);
 }
