6882d08cc970a034ba9c435cf09421008512b316
Your Name <you@example.com>
2024-01-19 17:08:56 +0000

    termin bs

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
 
 int main(void)
 {
-        char c = 'a';
+	char c = 'a';
 	char d = 'A';
 
-        while (c <= 'z')
-        {
-                putchar(c);
-                c++;
-        }
+	while (c <= 'z')
+ 	{
+		putchar(c);
+		c++;
+	}
 	while (d <= 'Z')
 	{
 		putchar(d);
 		d++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
