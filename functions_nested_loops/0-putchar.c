80931e4e42a4c9cf8f3237230aeaa219bd996b6d
Your Name <you@example.com>
2024-01-19 20:11:08 +0000

    added and activated 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main - write a program that prints putchar
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#This is a readme file for the Functions and nested loops directory. We are learning at a snails pace and are suffering greatly but will persevere.
