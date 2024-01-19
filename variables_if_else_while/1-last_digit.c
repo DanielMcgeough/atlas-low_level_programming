4821695a11f626549661500ef8b37d1f2d46e727
Your Name <you@example.com>
2024-01-19 16:33:25 +0000

    removed bad stuff

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,18 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <conio.h>
 /** betty style doc for function main goes there 
 *main - trying to parse info about last digit in number
 */
 int main(void)
 {
-        int n, last_digit;
+        int n;
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-	scanf("%d", &n);
-	last_digit = n % 10;
-	printf("Last digit of %d is %d", n, last_digit);
+	printf("Last digit of %d is %d", n, n % 10);
         if (n % 10 > 5)
         {
             	printf("and is greater than 5");
@@ -25,5 +22,6 @@ int main(void)
         {
         	printf("and is less than 6 and not 0");
         }
+
         return (0);
 }
