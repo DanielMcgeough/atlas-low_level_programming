8366027ed04725a7b5eb393dc346bd8dcf6d972b
Your Name <you@example.com>
2024-01-19 15:26:14 +0000

    trying to represent int

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@ int main(void)
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         printf("Last digit of %d is %d/10",(n),(n));
-        if (%d % 10 > 5)
+        if (n % 10 > 5)
         {
             	printf("and is greater than 5");
         }
-        else if (%d % 10 == 0,(n))
+        else if (n % 10 == 0,(n))
         {
             	printf("and is 0");
         }
