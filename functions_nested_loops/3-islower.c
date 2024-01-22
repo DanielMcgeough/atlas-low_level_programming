d85acc19b11e98ae42af412129b9cf4c947f43dc
Your Name <you@example.com>
2024-01-22 16:34:17 +0000

    added extra equals

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * int _islower(int c)- returns 1 if c is lowercase.
+ * int _islower(int c)- returns 1 if char is lowercase.
  *
  * Return: 0.
  */
 int _islower(int c)
 {	
-	if (c = 'a')
+	if (c == 'a')
 	{
 		return = 1;
 	{
-	else (c = 'A')
+	else (c == 'A')
 	{
 		return = 0;
 	}
