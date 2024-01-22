ce37008e9653b6ecf3ee52ce6e337ec754e383d7
Your Name <you@example.com>
2024-01-22 21:04:26 +0000

    fixed doc

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *print_to_98 - prints form 0 to 98
+ *@n: initial variable
+ *@i: new values for counting
+ *Return: Always 0
  */
 void print_to_98(int n)
 {
