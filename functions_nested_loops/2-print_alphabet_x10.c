8b9edd7c19cc6d17f98917f8a50a0b9d038742bf
Your Name <you@example.com>
2024-01-22 15:51:03 +0000

    declaration inside loops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10
+ * print_alphabet_x10 - prints the alphabet 10 times.
  */
 void print_alphabet_x10(void)
 {
-	char c = 'a';
-	int i = 0;
 
-	for (int i; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for (char c; c <= 'z'; c++)
+		for (char c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
