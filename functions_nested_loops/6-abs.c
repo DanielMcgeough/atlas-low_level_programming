a2314abc9d61719f9dcb342d60531c08c0dd034e
Your Name <you@example.com>
2024-01-22 18:05:20 +0000

    added and activated 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the abs val of an int.
+ *
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		return (-int);
+	}
+	else
+	{
+		return (int);
+	}
+return (0);
+}	
