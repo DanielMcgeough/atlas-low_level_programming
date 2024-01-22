293fb645c3f8298fea09158977b043da787882bb
Your Name <you@example.com>
2024-01-22 20:47:15 +0000

    added and activated 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int add - adds two numbers and returns sum
+ *@a: integer submitted
+ *@b: integer submitted
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * times_table - shows the multiplication tables
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+	int i, j, mult;
