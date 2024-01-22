00ff407446e1cc6c0cb2b637eb22eef26e735c68
Your Name <you@example.com>
2024-01-22 15:19:51 +0000

    added and activated 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * main - print alphabet using _putchar prototype.
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
