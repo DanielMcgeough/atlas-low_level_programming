0ab12286ed8503bdce8eac6a618fd69c87b0a748
Your Name <you@example.com>
2024-01-22 17:39:15 +0000

    added and activated 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * sign - based on if n is pos, neg, or eq to 0
+ *@n: int input
+ * Return: 1 if pos, 0 if eq, -1 if neg
+ */
+int print_sign(int n);
+{
+	if(n > 0)
+	{
+		return (1);
+	}
+	else if (n = 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+return (0);
+}
