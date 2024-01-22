17fc9e34ba4b26108f1691ddf3c6cfce2dc5d2eb
Your Name <you@example.com>
2024-01-22 16:58:11 +0000

    added and activated 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - will test if something is a letter or not.
+ * @c: checked input
+ * Return: return 1 if letter or else 0.
+ */
+int _isalpha(int c);
+{
+	if (c == 'a' || c =='A')
+	{
+		return (1);
+	}
+	else if
+	{
+		return (0);
+	}
+return (0);
+}
