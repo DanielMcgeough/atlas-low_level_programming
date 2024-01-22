165b853b7ce8b2699f484ff0ab67d37c269eaabc
Your Name <you@example.com>
2024-01-22 16:27:32 +0000

    added and activated 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _islower(int c)- returns 1 if c is lowercase.
+ *
+ * Return: 0.
+ */
+int _islower(int c);
+{
+	int c = 'a'
+	
+	if (c = 'a')
+	{
+		return = 1;
+	{
+	if else (c = 'A')
+	{
+		return = 0;
+	}
+}
