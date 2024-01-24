3862a281408f5d155dab0cbde30108cc48005d63
Your Name <you@example.com>
2024-01-24 17:13:40 +0000

    added and activated 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers- print number from 0-14 times 10
+ *
+ * Return: return is void
+ */
+void more_numbers(void);
+{
+	int a = 0;
+	while (a >= 0 && a <= 14)
+	{
+		_putchar(a * 10)
+		a++
+	}
+	_putchar('\n')
+}
