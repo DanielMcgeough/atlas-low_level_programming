4d8275fff5d6ee470b99d37e74a5ad472464c8e5
Your Name <you@example.com>
2024-01-24 16:41:43 +0000

    added and activated 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers- prints the numbers
+ *
+ * Return: Always Returns 0
+ */
+void print_numbers(void)
+{
+	int a = 0;
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
