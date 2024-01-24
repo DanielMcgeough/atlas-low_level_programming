569626363149a43576b9638435e7fe6521e5e76f
Your Name <you@example.com>
2024-01-24 15:38:09 +0000

    added and activated 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper- Checks to see if something is an uppercase char
+ *@c: int input
+ * Return: 1 if uppercase, 0 if otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A')
+	{
+		return (1);
+	{
+	else if (c == 'a')
+	{
+		return (0);
+	{
+	return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a README file in the Atlas low level programming repository
+with the directory devoted to learning more about functions and nested loops with a main file which means we dont get to use standard C libraries.
