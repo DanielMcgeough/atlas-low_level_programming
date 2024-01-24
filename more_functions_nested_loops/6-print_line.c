027cad6ca7690190c1344426732821ac0117b7f9
Your Name <you@example.com>
2024-01-24 19:11:53 +0000

    added and activated 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line- make the function print something
+ * @n: input number
+ *
+ * Return: return is void
+ */
+void print_line(int n)
+{
+
+	while (n > 0)
+	{
+		_putchar((95 + n)'\n')
+		if (n <= '0')
+		{
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
