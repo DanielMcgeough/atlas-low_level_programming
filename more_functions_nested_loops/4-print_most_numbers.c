57d99c02b0da709ba57d3c7c73e264e9aca8653d
Your Name <you@example.com>
2024-01-24 16:54:04 +0000

    added and activated 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers- print numbers from 0-9 without 2 and 4
+ *
+ * Return: return is void
+ */
+void print_most_numbers(void)
+{
+	int a =0;
+
+	while (a < 10 && a != '2' && a != '4')
+	{
+		_putchar('0' + a);
+		a++;
+	}
+	_putchar('\n');
+}
