5413f8b803b5a14c81ad181908d487edc03def64
Your Name <you@example.com>
2024-01-24 17:02:27 +0000

    added if

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,13 @@ void print_most_numbers(void)
 {
 	int a =0;
 
-	while (a < 10 && a != 2 && a != 4)
+	while (a < 10)
 	{
-		_putchar(48 + a);
-		a++;
+		if (a != 2 && a != 4)
+		{
+			_putchar(48 + a);
+			a++;
+		}
 	}
 	_putchar('\n');
 }
