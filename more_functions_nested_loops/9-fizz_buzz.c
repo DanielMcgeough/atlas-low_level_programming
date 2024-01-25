1be72fd0868b8b60d807b0cf283a832f8273fa00
Your Name <you@example.com>
2024-01-25 16:09:11 +0000

    more equals

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ void Fizz_Buzz(void)
 		{
 			printf("fizz");
 		}
-		else if (a % 5 = 0)
+		else if (a % 5 == 0)
 		{
 			printf("buzz");
 		}
-		else if (a % 3 = 0 && a % 5 = 0)
+		else if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("fizzbuzz");
 		}
