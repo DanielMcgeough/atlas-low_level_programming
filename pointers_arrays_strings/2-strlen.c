51bb434dc17297c8ede3a32d551081ddfbb0f487
Your Name <you@example.com>
2024-01-26 17:45:15 +0000

    added and activated 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen- returns length of string
+ * @s: string
+ *
+ * Return: length of string is returned
+ */
+int _strlen(char *s);
+{
+	char str[] = s;
+	int length = str;
+	return (length);
+}
