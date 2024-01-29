f8b720f556d85370920fcda9f703f84ee79846e5
Your Name <you@example.com>
2024-01-29 18:18:33 +0000

    added and activated 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array- print an array of n length.
+ * @a: integer passed
+ * @n: integer tied to length
+ *
+ * Return: return is void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for(i = 0; i < n; i++)
+	{
+		if (i <= (n - 1))
+			printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
