38874591e0cdc4a8462a633bf32ae15aa154b44a
Your Name <you@example.com>
2024-01-29 21:02:07 +0000

    added 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _atoi- changes a string to an integer
+ * @s: character fed into func
+ *
+ * Return: Returns an int
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int firstnum = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			(sign = sign * -1);
+		}
+		else if (*s >= 48 && *s <= 57)
+		{
+			result = result * 10 + (*s - 48);
+		}
+		else if (*s == '+' || *s == ' ')
+		{
+
+		}
+		else if (firstnum == 1)
+		{
+			break;
+		}
+		s++;
+	}
+	return (result * sign);
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *_strcpy- copies a string with null
+ * @dest: passed in character as pointer
+ * @src: passed in character also as pointer
+ *
+ * Return: return is character
+ */
+char *_strcpy(char *dest, char *src)
+
