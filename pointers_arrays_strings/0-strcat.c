75990c6bbcd384b1347f68b3d21ccfd0091186c9
Your Name <you@example.com>
2024-01-31 18:08:30 +0000

    added and activated new 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcat- function to concatenate two strings
+ * @dest: string passed in
+ * @src: other string passed in
+ *
+ * Return: Returns a char printing new string
+ */
+char *_strcat(char *dest, char *src)
+{
+	(*dest + *src) = *_strcat;
+	return (_strcat);
+}
