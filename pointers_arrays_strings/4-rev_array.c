97cfe4bbb9d7a316c1a6524d00c3d62b9d46b1cd
Your Name <you@example.com>
2024-02-01 18:18:34 +0000

    added 4 with for loop

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rev_array- function that reverses array of ints
+ * @a: pointer to int
+ * @n: int passed in
+ *
+ * Return: return is void
+ */
+void reverse_array(int *a, int n)
+{
+	for (int i = 0; i < n/2; i++)
+	{
+		int temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n -1 -i] = temp;
+	}
