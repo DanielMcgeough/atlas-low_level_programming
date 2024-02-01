fbd67a2383c07d7bfa495340adae83a4d9429083
Your Name <you@example.com>
2024-02-01 19:20:47 +0000

    added and activated 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper- changes lower to upper case
+ *
+ * @*: pointer to a char
+ * Return: Returns a char
+ */
+char *string_toupper(char *abigail)
+{
+	int i;
+
+	for(i = 0; abigail[i] != '\0'; i++)
+	{
+		if(abigail[i] >= 97 && abigail[i] <= 122)
+		{
+		abigail[i] = abigail[i] - 32;
+		}
+	}
+{
