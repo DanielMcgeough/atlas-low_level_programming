aefdc18631f43cd386b88a7117205170115c214f
Your Name <you@example.com>
2024-02-01 16:02:40 +0000

    first draft of 3

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strncpy- function to copy a string
+ * @dest: string passed in
+ * @src: other string passed in
+ * @n: int passed in
+ *
+ * Return: Returns a char printing new string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		src[b] = dest[a]
+	}
+	src[b] = '\0';
