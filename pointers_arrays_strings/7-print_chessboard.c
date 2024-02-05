29ca8cc2072713e06cd816a6745a8bcfe153d921
Your Name <you@example.com>
2024-02-05 19:58:43 +0000

    second try chess

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -10,11 +10,11 @@ void print_chessboard(char (*a)[8])
 	int row = 0;
 	int column = 0;
 
-	for (row = 0; a[row] < 8; row++)
+	for (row = 0; row < 8; row++)
 	{
-		for (column = 0; a[column] < 8; column++)
+		for (column = 0; column < 8; column++)
 		{
-			putchar (a[row],[column];
+			putchar (a[row],[column]);
 		}
 	}
 }
