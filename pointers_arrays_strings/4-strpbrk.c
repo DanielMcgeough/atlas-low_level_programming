82f9fdcc07625bd43c43ff1f435ee41580add0ea
Your Name <you@example.com>
2024-02-05 17:53:54 +0000

    added spaces

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,14 +10,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while(*s)
+	while (*s)
 	{
-		for(i = 0; accept[i]; i++)
+		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 			return (s);
 		}
 	s++;
 	}
-	return('\0');
+	return ('\0');
 }
