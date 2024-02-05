f0fb4c23542362884775200d9e60ab38f5d73eb3
Your Name <you@example.com>
2024-02-05 15:12:27 +0000

    first unsigned int ret, simple otherwise

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strspn- get length of prefix substring
+ * @s: array of char as string.
+ * @accept: array of char as string also.
+ *
+ * Return: returns an unsigned int.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+
+	while (*s != '\0' && *accept != '\0' && *s == *accept)
+	{
+		length++;
+		s++;
+		accept++;
+	}
+	return length;
+}
