b6d06b4d8f97e82caa1454d89ba18cefc1fe9912
Your Name <you@example.com>
2024-02-08 02:45:17 +0000

    start on 1

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts- prints a string
+ * @str: input character
+ *
+ * Return: returns string
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if(s[i] < (*s != '\0') 
+	{
+	}	       
+	return s[i];
+	i++;
+	_putchar('\n');
+}
