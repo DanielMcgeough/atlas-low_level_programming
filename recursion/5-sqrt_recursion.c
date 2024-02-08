34691887535df41229452f08b96b1c9954eecebd
Your Name <you@example.com>
2024-02-08 17:57:36 +0000

    wrecked

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** _sqrt_recursion- returns nat sqrt of a number
+ * @n: number passed in
+ *
+ * Return: returns an int
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	else if (scrt == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
