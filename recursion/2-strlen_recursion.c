5543640809de1a1eaaa40bae95a00b80ac8a306d
Your Name <you@example.com>
2024-02-08 16:53:52 +0000

    bring it

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/** _strlen_recursion- returns length of string
+ * @s: string passed in
+ *
+ * Return: returns an int of length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		retrun 1 + _strlen_recursion(s + 1);
+	}
+}
