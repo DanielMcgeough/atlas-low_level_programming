b7cbec5d22c8a4b73e96ed7e005f2601740c92d5
Your Name <you@example.com>
2024-02-09 17:30:11 +0000

    trying it out

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-'
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: return an int.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n");
+	(void) argv[];
+
+	return (0);
+}
