30e3de509f3034c2db8ad68157c13301af678fa3
Your Name <you@example.com>
2024-02-09 17:47:37 +0000

    trying a for loop

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * main- prints all args it receives
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: return is an int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	(void) argc;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+	
+	
