3c7f344ae4122cef08f42c6ff124ae167b6c9680
Your Name <you@example.com>
2024-02-09 19:46:53 +0000

    setup skelly

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- adds positive numbers
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: will return int
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+
+	for(a = 0; a < argc; a++)
+	{
+		if (argv[a] < 48 || > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (argc < 2)
+		{
+			printf("0\n");
+		}
+		else
+		{
+			argv[a] + 
+		}
+	}
+	return (0);
+}
+
+
