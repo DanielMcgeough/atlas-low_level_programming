c30cd9181ad299ff2b77afc5a9e8fa37add90615
Your Name <you@example.com>
2024-02-09 17:58:53 +0000

    added it

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- multiplies two numbers
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Return: product in int
+ */
+int main(int argc, char *argv[])
+{	
+	int a;
+	int b;
+	int c;
+
+	printf("%d * %d = %d", a b c);
+	return (c);
+}
