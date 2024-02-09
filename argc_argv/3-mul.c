3d8613c565e7089aece16cbe7ef9ac6e962a1a37
Your Name <you@example.com>
2024-02-09 19:13:15 +0000

    reorder

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,15 +10,17 @@
  */
 int main(int argc, char *argv[])
 {	
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a;
+	int b;
 
 	if (argc != 3)
 	{
 		printf("error\n");
 		return (1);
 	}
-
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	printf("%d\n", a * b);
 	return (0);
 }
