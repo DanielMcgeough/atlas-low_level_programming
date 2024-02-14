3d8d093a353bf34925a86ba328ef17b86a4ca0c6
Your Name <you@example.com>
2024-02-14 22:42:45 +0000

    created array, needed help for a bit of it

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup: return pointer to new space in mem with str as param
+ * @str: the pointer to the string
+ *
+ * Return: null or success
+ */
+char *_strdup(char *str);
+{
+	int a = 0;
+	int b = 1;
+	char *s;
+
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	while (str[b])
+	{
+	b++
+	}
+
+	s = malloc((sizeof(char) * b) + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while (a < b)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = '\0';
+	return (s);
+}
