d010853cb6d3f972be189a7126531a2684a57d8d
Your Name <you@example.com>
2024-02-14 22:25:14 +0000

    create array with malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array- create an array and init it
+ * @size: size of array
+ * @c: char passed into func
+ *
+ * Return: returns a pointer or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+	{
+		Return (NULL);
+	}
+
+	s = malloc(sizeof(char) * size);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
+}
