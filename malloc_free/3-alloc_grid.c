4627edbc56862090b0cf8c8095f9da1b5535f07e
Your Name <you@example.com>
2024-02-14 23:50:37 +0000

    cant let it go

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid- point to a 2d array of ints
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: returns an int
+ */
+int **alloc_grid(int width, int height);
