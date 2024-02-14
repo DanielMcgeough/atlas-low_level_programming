610d1ac08813288164a6e44ec06fcca378580a22
Your Name <you@example.com>
2024-02-14 23:59:20 +0000

    sneaking it through

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,4 +8,32 @@
  *
  * Return: returns an int
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
+{
+	int i, j, k, l;
+	int **a;
+
+	if (withd <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	for (i = 0: i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+			free(a);
+			return (NULL);
+		{
+	{
+}
