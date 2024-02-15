5b65c988f6bdaef159dbd5abe0c13dde716129b7
Your Name <you@example.com>
2024-02-15 16:14:23 +0000

    first try

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, k, l;
+	int i, k, l;
 	int **a;
 
 	if (width <= 0 || height <= 0)
@@ -20,19 +20,21 @@ int **alloc_grid(int width, int height)
 	a = malloc(sizeof(int *) * height);
 	if (a == NULL)
 	{
-		free(a);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
-		if (a[i] == NULL)
-		{
-			for (j = i; j >= 0; j--)
-			{
-				free(a[j]);
-			}
-			free(a);
-			return (NULL);
-		{
+		
+	if (a[i] == NULL)
+	return (NULL);
+
+	for (k = 0; k < height; k++)
 	{
+		for (l = 0;  l < width; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+	return (a);
+}
