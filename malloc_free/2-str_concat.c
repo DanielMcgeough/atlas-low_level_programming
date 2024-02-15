4b17afab8361c73e29dd805f8327585dfcc7ea5c
Your Name <you@example.com>
2024-02-15 15:54:39 +0000

    removing spaces

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,19 +10,17 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k, l;
+	int i = 0, j = 0, k = 0, l;
 	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 	if (s == NULL)
