48c9a7ee10085cc853a120c33376d118fd83f04f
Your Name <you@example.com>
2024-02-16 15:34:21 +0000

    starting simple

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked- allocates memmory using malloc
+ * @b: an unsigned int passed in. probably to hold the pointer
+ * Return: returns a void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int a;
+
+	a = malloc(b);
+
+	if (b == NULL)
+	{
+		exit(98);
+	}
+	return (a)
+}
