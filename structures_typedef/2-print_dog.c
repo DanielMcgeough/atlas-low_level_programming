35be3703404e9e0069287f981598d0e48a7967c5
Your Name <you@example.com>
2024-02-21 16:12:24 +0000

    annoying syntax

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - print info about the pupper.
+ * @d: pulling in the struct for stuff
+ * Return: return is a void
+ */
+void print_dog
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name ? d->name : "(nil)");
+		printf("Age: %d\n", (d->age ? d->age : "(nil)");
+		printf("Owner: %s\n", (d->owner ? d->owner : "(nil)");
+	{
+}
