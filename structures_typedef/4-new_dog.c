177020349fc8a23cfa7d9a45142f920669853ff5
Your Name <you@example.com>
2024-02-21 17:06:19 +0000

    reaffirm

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,73 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - making a new entry for the diff pupper
+ * @name: name of pupper
+ * @age: age of pupper
+ * @owner: name of owner
+ *
+ * Return: returns stuff
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+char *_strcpy(char *dest, char *src)
+	{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *mooshu_dog;
+	int namey = 0, ownery = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+		namey = _strlen(name) + 1;
+		ownery = _strlen(owner) + 1;
+		mooshu_dog = malloc(sizeof(dog_t));
+
+		if(mooshu_dog == NULL)
+			return (NULL);
+
+		mooshu_dog->name = malloc(sizeof(char) * namey);
+
+		if (mooshu_dog->name == NULL)
+		{
+			free(mooshu_dog);
+			return (NULL);
+		}
+
+		mooshu_dog->owner = malloc(sizeof(char) * ownery);
+
+		if (mooshu_dug->owner == NULL)
+		{
+			free(mooshu_dog->name);
+			free(mooshu_dog);
+			return (NULL);
+		}
+
+		mooshu_dog->name = _strcpy(mooshu_dog->name, name)
+		mooshu_dog->owner = _strcpy(mooshu_dog->owner, owner);
+		mooshu_dog->age = age;
+	}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -24,5 +24,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
 #endif
