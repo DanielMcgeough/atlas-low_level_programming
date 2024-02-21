5e07ee74f7cde77a24ef6cd1e9c01cefd62ced43
Your Name <you@example.com>
2024-02-21 16:29:58 +0000

    this is bananas

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,7 +16,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - new typedef utilizing struct dog
+ */
+
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
