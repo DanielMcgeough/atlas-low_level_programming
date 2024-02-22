729894621425a8ea5dc4d24b861ce511a3463e57
Your Name <you@example.com>
2024-02-22 18:08:53 +0000

    first try

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: separates the strings
+ * @n: an int number of strings
+ * @...: the actual strings themselves.
+ */
+void print_strings(cont char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	char *str;
+	unsigned int i;
+
+	 va_start(strings, n);
+
+	 for (i = 0; i < n; i++)
+	 {
+		 str = va_arg(strings, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+
+	if (i != (n - 1) && separator != NULL)
+		printf("%s"m separator);
+	 }
+
+	 printf("\n");
+
+	 va_end(strings);
