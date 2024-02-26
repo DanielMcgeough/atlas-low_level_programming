3e4586c4d49d71c31a2fbfb45fec9d21f04c16b1
Your Name <you@example.com>
2024-02-26 21:10:01 +0000

    legit crazy

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,54 +1,101 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * print_all - self explanatory
- * @format: list of args
+ * @format: list of formats in form of args
+ *
  * Return: returns void
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str = "";
-	char *sep = "";
+       va_list args;
+	f_dt form_types[] = {
+		{ "c", print_a_char },
+		{ "i", print_a_integer },
+		{ "f", print_a_float },
+		{ "s", print_a_char_ptr }
+	};
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
 
-	va_list list;
+	va_start(args, format);
 
-	va_start(list, format);
-
-	if (format)
+	while (format != NULL && format[i])
 	{
-		while (format[i])
+		j = 0;
+		while (j < 4)
 		{
-			switch (format[i])
+			if (format[i] == *form_types[j].identifier)
 			{
-				case 'c':
-				printf("%s%c", sep, va_arg(list, int));
-				break;
-				case 'i':
-				printf("%s%d", sep, va_arg(list, int));
-				break;
-				case 'f':
-				printf("%s%f", sep, va_arg(list, double));
-				break;
-				case 's':
-				str = va_arg(list, char *);
-				if (!str)
-				{
-					str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				}
-				default:
-					i++;
-					continue;
+				form_types[j].f(separator, args);
+				separator = ", ";
 			}
-			sep = ", ";
-			i++;
+			j++;
 		}
+		i++;
 	}
+
+	va_end(args);
 	printf("\n");
-	va_end(list);
 }
-	
+
+/**
+ * print_a_char - prints a char
+ * @separator: the separator
+ * @args: list of args
+ *
+ * Return is void
+ */
+void print_a_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+/**
+ * print_a_integer - prints an int
+ * @separator: separator of int
+ * @args: list of args
+ *
+ * Return: returns a void
+ */
+void print_a_integer(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+{
+
+/**
+ * print_a_float - prints a floating num
+ * @separator: the separator
+ * @args: list of args
+ * 
+ * Return: returns a void
+ */
+void print_a_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+/**
+ * print_a_char_ptr - prints a char pointer
+ * @separator: the separator
+ * @args: list of args
+ *
+ * Return: returns a void
+ */
+void print_a_char_ptr(char *separator, va_list args)
+{
+	char *arg = va_arg(args, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, arg);
+}
