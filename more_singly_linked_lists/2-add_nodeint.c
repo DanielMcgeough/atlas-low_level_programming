613e7f9ca994593d66b2ff291087f40cfd0bec22
Your Name <you@example.com>
2024-03-07 04:38:13 +0000

    this makes sense finally

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint- adds a new node at beg of list
+ * @**head: list of nodes
+ * @n: int of number
+ * Return: return address of new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL):
+	
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
+}
