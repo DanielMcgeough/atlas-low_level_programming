3fd8651553b5cdaf6a1a7028a4b2dcf5bb67138f
Your Name <you@example.com>
2024-03-07 01:24:04 +0000

    wrong termin

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	newNode = malloc(sizeof(list_t));
 	if (!newNode)
-		return (NULL):
+		return (NULL);
 
 	newNode->str = strdup(str);
 	newNode->i = i;
