817c4d5da54d5ae8626a55ad6ad8e2de833fdb69
Your Name <you@example.com>
2024-03-07 01:21:30 +0000

    ace needed less

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * *add_node- adds new node at beg of list
+ * @head: array of names
+ * @str: new node to add
+ * Return: return address of new element or NULL
+ */
+list_t *add_node(list_t **head, const char *str);
+{
+	list_t *newNode;
+	unsigned int i = 0;
+
+	while (str(i))
+		i++;
+
+	newNode = malloc(sizeof(list_t));
+	if (!newNode)
+		return (NULL):
+
+	newNode->str = strdup(str);
+	newNode->i = i;
+	newNode->next = (*head);
+	(*head) = newNode;
+
+	return (*head);
+}
