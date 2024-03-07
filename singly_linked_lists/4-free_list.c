d7be872e50987c8368442113c4f374002bc3c132
Your Name <you@example.com>
2024-03-07 01:44:49 +0000

    not cool

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list- frees a list
+ * @head: list
+ * Return: return is void
+ */
+void free_list(list_t *head)
+{
+	list_t *whatsasm
+
+	while (head != NULL)
+	{
+		whatasm = head;
+		head = head->next;
+		free(whatsasm->str);
+		free(whatsasm);
+	}
+}
