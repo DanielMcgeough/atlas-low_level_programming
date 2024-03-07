285c18f310c623be0257f72d6317f502bfcc2e0a
Your Name <you@example.com>
2024-03-07 18:33:12 +0000

    parentheses and termin added

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (!temp || !(temp->next))
 			return (-1);
 		temp = temp->next;
-		a++
+		a++;
 	}
 
 	current = temp->next;
