94e09e30dde56cec01782974ec771c966439474a
Your Name <you@example.com>
2024-03-07 04:52:23 +0000

    tough road

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint_end- function to add new node to end
+ * @head: list of nodes;
+ * @n: number of nodes;
+ * Return: return address
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode;
+	listint_t *temp = *head;
+
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+	temp ->next = newNode;
+
+	return (newNode);
+}
