4f2879e90d39b722109b1886287af30fc8c3dcff
Your Name <you@example.com>
2024-03-07 01:06:53 +0000

    this is brutal

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len- a function that tells you the number of elements
+ * @h: number of elements
+ * Return: returns size
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	if (node == NULL)
+		return (0);
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
