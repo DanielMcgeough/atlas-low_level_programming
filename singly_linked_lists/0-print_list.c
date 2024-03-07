d6bc7c0ac28c391a08a054c00bcda00242cb2534
Your Name <you@example.com>
2024-03-07 00:55:38 +0000

    rework

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,16 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t num = 0;
+	int i = 0;
 	
 	if (h == NULL)
 		return (0);
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		num++;
+		printf("[%d] ", h->len);
+		if (h->str)
+			printf("%s\n", h->str);
+		else
+			printf("(nil)\n";
+		i++
 		h = h->next;
 	}
-	return (num);
+	return (i);
 }
