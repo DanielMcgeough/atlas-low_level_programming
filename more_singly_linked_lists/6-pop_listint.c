ab45d68984711c897404d378d4984e02782f904f
Your Name <you@example.com>
2024-03-07 15:53:20 +0000

    first try of this

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint- delete head and take its data
+ * @head: linked list of nodes
+ * Return: returns an int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free (*head);
+	*head = temp;
+
+	return (num);
+}
