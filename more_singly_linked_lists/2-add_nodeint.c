c1959e381ce801c1fb24c56e72b5b042251a24a6
Your Name <you@example.com>
2024-03-07 04:40:33 +0000

    whitespace

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
 		return (NULL);
-	
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
