ef101edb4c1f9b4e9dd71e2b6e6b2faefb2de97c
Your Name <you@example.com>
2024-03-08 19:56:53 +0000

    fixed whitespace and dox

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -4,12 +4,13 @@
  * set_bit- sets the value of a bit to a 1 at index
  * @n: some long int to change
  * @index: pos of int to change
+ * Return: returns an int
  */
 int set_bit(unsigned long int *n, unsigned int index)
-{	
+{
 	if (index > 63)
 	return (-1);
-	
+
 	*n = ((1UL << index) | *n);
 	return (1);
 }
