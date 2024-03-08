b40ac4886f8c9d810555fd8699ef4e8256852382
Your Name <you@example.com>
2024-03-08 19:55:45 +0000

    somewhat magic

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_bit- sets the value of a bit to a 1 at index
+ * @n: some long int to change
+ * @index: pos of int to change
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{	
+	if (index > 63)
+	return (-1);
+	
+	*n = ((1UL << index) | *n);
+	return (1);
+}
