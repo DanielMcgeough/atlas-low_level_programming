9107eb1df67a38a6d744d1f64a3bfc0d4e4e6006
Your Name <you@example.com>
2024-03-11 22:18:15 +0000

    working it

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 		while ((text_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 		{
 			text_written = write(fd_to, buffer, text_read);
-			if (text_written != text_read)
+				if (text_written != text_read)
 				{
 					dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 					close(fd_from);
