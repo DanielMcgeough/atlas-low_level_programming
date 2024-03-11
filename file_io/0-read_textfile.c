f9cb5e1b4f5262573688ef83af81f140959479b8
Your Name <you@example.com>
2024-03-11 21:27:01 +0000

    using a lot of stupid stuff

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile- a function that reads a file and prints it to stdout
+ * @filename: naem of the files
+ * @letters: the letters to print
+ * Return: returns an output of ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *oof;
+	ssize_t filedes;
+	ssize_t w;
+	ssize_t t;
+
+	filedes = open(filename, O-RDONLY);
+	if (filedes == -1)
+		return (0);
+	oof = malloc(sizeof(char) * letters);
+	t = read(fildes, oof, letters);
+	w = write(STDOUT_FILENO, oof, t);
+
+	free(oof);
+	clsoe(fildes);
+	return (w);
+}
