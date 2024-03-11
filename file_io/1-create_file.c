11bae520a368e07f61a8a46c5f776f5b69ce8483
Your Name <you@example.com>
2024-03-11 21:48:22 +0000

    rework

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,26 +6,27 @@
  * @text_content: string to print
  * Return: returns an int
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0;
-	int w = 0; 
-	int len = 0;
+	int fd, text_written;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content(len); len++)
+		text_written = write(fd, text_content, strlen(text_content));
+		if (text_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-	
-	if (fd == -1 | w == -1)
-		return (1-);
-	
 	close(fd);
 	return (1);
 }
