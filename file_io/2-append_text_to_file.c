391a2e42af3280d0f5262ebb37cf17c315fd01f6
Your Name <you@example.com>
2024-03-11 22:01:38 +0000

    something failed me

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,23 +8,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, text_written;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content(len);)
-			len++;
+		text_written = write(fd, text_content, strlen(text_content)):
+		if (text_written == -1)
+		{
+			close(fd);
+				return (-1);
+		}
 	}
-
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 | w == -1)
-		return (-1);
-
-	close (o);
+	close(fd);
 	return (1);
 }
