fdbe3da7b9abe67c2644cc93bd7a608498bf1b9e
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 22:08:51 +0000

    These word soups get confusing at times

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint- add a node to beginning
+ * @head: pointer to head of list
+ * @n: new node value
+ * Return: returns the address of new element
+*/
+dlistint_t *add_dnodeint(dlist int_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+    	return (NULL); 
+
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+	if (h != NULL)
+		h->prev = new;
+	
+	*head = new;
+
+	return (new);
+}
