d9e81e18827845b4b4e688dc9cbeed5406ece017
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 22:59:50 +0000

    forgot termin

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
         return (NULL);
     
     while (head->prev != NULL)
-        head = head->prev
+        head = head->prev;
 
     while (head != NULL)
     {
