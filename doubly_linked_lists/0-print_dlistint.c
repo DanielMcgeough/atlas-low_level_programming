24076ac25b75b0759e2e37863284790da37e224d
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 21:30:30 +0000

    ooohwee!

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,19 @@
 
 /**
  * print_dlistint - prints all elements of list
- * @h: pointer to head of list
+ * @h: pointer to head of list might need another for spaces
  * Return: returns number of nodes in list
 */
 size_t print_dlistint(const dlistint_t *h)
 { int count = 0;
 
-    if (h == NULL)
-        return (count);
-        
-    while (h->prev != NULL)
-    h = h->prev;
+if (h == NULL)
+	return (count);
 
-    while (h != NULL)
+	while (h->prev != NULL)
+	h = h->prev;
+
+	while (h != NULL)
     {
         printf("%d\n", h->n);
         count++;
