94d1e7db7340b71291fe3295de645b33eff83c77
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 22:50:49 +0000

    needed a rework

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -8,13 +8,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
-	if (head != NULL)
-		while (head->prev != NULL)
-			head = head->prev;
-
-	while ((temp = head) != NULL)
-		{
-			head = head->next;
-			free(temp);
-		}
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
