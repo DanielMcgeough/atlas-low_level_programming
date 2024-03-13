ff0da03083caa4053c88671c09a71ebb2123d9a4
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 21:52:12 +0000

    Woop Woop

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct dlistint_s
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
