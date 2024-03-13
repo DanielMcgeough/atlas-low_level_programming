7277ba85077abf5054e21a12a90e53831ed728db
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 22:59:06 +0000

    Um why backwards first?

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at an index
+ * @head: linked list
+ * @index: index of node to return
+ * Return: returns the node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int a = 0;
+
+    if (head == NULL)
+        return (NULL);
+    
+    while (head->prev != NULL)
+        head = head->prev
+
+    while (head != NULL)
+    {
+        if (a == index)
+            break;
+        head = head->next;
+        a++;
+    }
+
+    return (head);
+}
