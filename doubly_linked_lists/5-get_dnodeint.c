4e71bbd56ec6e2520f82a647062a9ef609917acd
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 23:03:44 +0000

    spaces work

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,17 +8,17 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int a = 0;
+	unsigned int a = 0;
 
-    if (head == NULL)
-        return (NULL);
-    
-    while (head->prev != NULL)
-        head = head->prev;
+	if (head == NULL)
+		return (NULL);
 
-    while (head != NULL)
-    {
-        if (a == index)
+	while (head->prev != NULL)
+		head = head->prev;
+
+	while (head != NULL)
+	{
+		if (a == index)
 			break;
 		head = head->next;
 		a++;
