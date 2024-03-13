91430855cc7a3b3be6ac21e911e67f7a13499e50
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-03-13 22:17:20 +0000

    This stuff is wack

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a node to the end of list
+ * @head: pointer to head
+ * @n: value to put in new node
+ * Return: returns address of new
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+    dlistint_t *h;
+
+    if (new == NULL)
+        return (NULL);
+	
+	new->n = n;
+	new->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+	new->prev = h;
+
+	return (new);
+}
