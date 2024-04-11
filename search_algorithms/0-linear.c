d2c2e852e39e4cc8d206253e05f4861d5c495d30
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-04-11 22:27:05 +0000

    Included linear search with for loop and printf

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+* linear search- conducts a linear search through an array
+* @array: pointer to array
+* @size: size of array
+* @value: values to search for
+* Return: returns an int
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size, i++)
+	{
+		printf("Value found at index:[%d] = [%d]\n", i, array[i]);
+		if (array == value)
+			return (i);
+	}
+	return (-1);
+}
